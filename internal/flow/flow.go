d3761045af36c8017d033ec00aefe5410bdc4a17
Yuri Vishnevsky <yurivish@gmail.com>
2025-08-13 16:37:49 -0400

    checkpoint

--- internal/flow/flow.go
+++ internal/flow/flow.go
@@ -3,7 +3,6 @@ package flow
 import (
 	"context"
 	"fmt"
-	"sync"
 
 	"datapotamus.com/internal/msg"
 	"datapotamus.com/internal/pubsub"
@@ -39,24 +38,42 @@ func NewFlow(id string, stages []stage.Stage, conns []Conn, ps *pubsub.PubSub) *
 		out   chan msg.PortMessage
 	}
 
-	stagesById := map[string]StageConfig{}
+	stageConfigs := map[string]StageConfig{}
 	for _, s := range stages {
-		stagesById[s.ID()] = StageConfig{s, make(chan msg.PortMessage, 100), make(chan msg.PortMessage, 100)}
+		stageConfigs[s.ID()] = StageConfig{s, make(chan msg.PortMessage, 100), make(chan msg.PortMessage, 100)}
 	}
 
-	// what if we say flow ids have to be "flow.xxxx" - makes it easier to see in the suture log messages and we can use that as a component in the pubsub subjects
 	for _, c := range conns {
 		subj := fmt.Sprintf("flow.%s.stage.%s.port.%s", id, c.src.Stage, c.src.Port)
 		cancel := pubsub.Sub(ps, subj, func(subj string, m msg.Message) {
-			cfg := stagesById[c.dst.Stage]
+			cfg := stageConfigs[c.dst.Stage]
 			cfg.in <- msg.PortMessage{Port: c.dst.Port, Message: m}
 		})
 		_ = cancel // i think we should do all this in Serve
 	}
 
+	// var wg sync.WaitGroup
+
+	// wg.Go(func() {
+	// 	for range m := s.out {
+	// 		select {
+	// 		case m, ok := <-s.in:
+	// 			// if the input channel is closed then exit gracefully
+	// 			if !ok {
+	// 				return
+	// 			}
+
+	// 		case <-ctx.Done():
+	// 			return
+	// 		}
+	// 	}
+	// })
+	//
+	//
+
 	sv := suture.NewSimple(id)
 	for _, s := range stages {
-		cfg := stagesById[s.ID()]
+		cfg := stageConfigs[s.ID()]
 		s.Connect(cfg.in, cfg.out)
 		sv.Add(s)
 	}
@@ -66,32 +83,6 @@ func NewFlow(id string, stages []stage.Stage, conns []Conn, ps *pubsub.PubSub) *
 }
 
 func (f *Flow) Serve(ctx context.Context) error {
-	var wg sync.WaitGroup
 
-	wg.Go(func() {
-		for {
-			select {
-			case m, ok := <-s.in:
-				// if the input channel is closed then exit gracefully
-				if !ok {
-					return
-				}
-				results, err := s.Query(ctx, m.Data)
-				// send the error, if any
-				if err != nil {
-					s.out <- msg.PortMessage{Port: "error", Message: msg.Message{Data: err}}
-				} else {
-					// otherwise send the results, if any.
-					// todo: we could send partial results even in the face of an error, or
-					// even multiple errors, if we decide that is the behavior we want.
-					for _, result := range results {
-						s.out <- msg.PortMessage{Port: "out", Message: msg.Message{Data: result}}
-					}
-				}
-			case <-ctx.Done():
-				return
-			}
-		}
-	})
 	return nil
 }
